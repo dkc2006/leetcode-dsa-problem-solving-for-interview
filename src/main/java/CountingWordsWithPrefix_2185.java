deae9398887f7c90c5715fa9cfba8f9fac89d926
Deepanshu Khandelwal <122585913+dkc2006@users.noreply.github.com>
2025-01-09 21:21:08 +0530

    Problem 2185. Counting Words With a Given Prefix solution

--- src/main/java/CountingWordsWithPrefix_2185.java
+++ src/main/java/CountingWordsWithPrefix_2185.java
@@ -1,16 +1,32 @@
 class Solution {
     public int prefixCount(String[] words, String pref) {
-        int len = pref.length();
-        int words_len = words.length;
         int count = 0;
-        for (int i = 0; i < words_len; i++) {
-            if (words[i].length() >= len) {
-                String check_prefix = words[i].substring(0, len);
-                if (pref.equals(check_prefix)) {
-                    count++;
-                }
-            }
+        int len = words.length;
+        for (int i = 0; i < len; i++) {
+            if (words[i].startsWith(pref))
+                count++;
         }
         return count;
+
     }
 }
+
+//            OR
+
+//
+//class Solution {
+//    public int prefixCount(String[] words, String pref) {
+//        int len = pref.length();
+//        int words_len = words.length;
+//        int count = 0;
+//        for (int i = 0; i < words_len; i++) {
+//            if (words[i].length() >= len) {
+//                String check_prefix = words[i].substring(0, len);
+//                if (pref.equals(check_prefix)) {
+//                    count++;
+//                }
+//            }
+//        }
+//        return count;
+//    }
+//}
