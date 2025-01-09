3631ad2f92758c1d27bd175aa903169ee08de5ca
Deepanshu Khandelwal <122585913+dkc2006@users.noreply.github.com>
2025-01-09 20:24:53 +0530

    Problem 1422. Maximum Score After Splitting a String solution

--- src/main/java/MaximumScoreAfterSplittingaString_1422.java
+++ src/main/java/MaximumScoreAfterSplittingaString_1422.java
@@ -1,3 +1,18 @@
-public class MaximumScoreAfterSplittingaString_1422 {
-
+class Solution {
+    public int maxScore(String s) {
+        int length = s.length();
+        int ones = 0;
+        for(int i=0;i<length;i++){
+            if(s.charAt(i) == '1') ones++;
+        }
+        // find max partition
+        int res = 0;
+        int zeros = 0;
+        for(int i=0;i<length-1;i++){
+            if(s.charAt(i) == '0')  zeros++;
+            else ones--;
+            res = Math.max(res,ones+zeros);
+        }
+        return res;
+    }
 }
