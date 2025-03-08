7599f80ed2ca74cd6f849f60647460ff28965174
Deepanshu Khandelwal <122585913+dkc2006@users.noreply.github.com>
2025-03-08 21:37:38 +0530

    Problem 2379. Minimum Recolors to Get K Consecutive Black Blocks solution

---
+++ src/main/java/MinimumRecolorstoGetKConsecutiveBlackBlocks_2379.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minimumRecolors(String blocks, int k) {
+        // T.C - > O(N)
+        int w = 0;
+        for (int i = 0; i < k; i++) {
+            if (blocks.charAt(i) == 'W')
+                w++;
+        }
+        int res = w;
+        int n = blocks.length();
+        for (int i = 1; i < (n - k + 1); i++) {
+            if (blocks.charAt(i - 1) == 'W')
+                w--;
+            if (blocks.charAt(i + k - 1) == 'W')
+                w++;
+            res = Math.min(res, w);
+        }
+        return res;
+    }
+}
