71f874f21a8afd97082465ba4e6b818b30fa4be6
Deepanshu Khandelwal <122585913+dkc2006@users.noreply.github.com>
2025-05-28 13:08:22 +0530

    Problem \1721. Swapping Nodes in a Linked List solution

---
+++ src/main/java/SwappingNodesinaLinkedList_1721.java
@@ -0,0 +1,19 @@
+class Solution {
+    public ListNode swapNodes(ListNode head, int k) {
+        ListNode slow = head,fast = head;
+        ListNode start = head,end = head;
+        for(int i=1;i<k;i++){
+            fast = fast.next;
+        }
+        start = fast;
+        while(fast.next != null){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        end = slow;
+        int temp = start.val;
+        start.val = end.val;
+        end.val = temp;
+        return head;
+    }
+}
