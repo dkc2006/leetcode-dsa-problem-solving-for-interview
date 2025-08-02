1895cbc6afa5304eb0bf2053f83852bccfc1e031
Deepanshu Khandelwal <122585913+dkc2006@users.noreply.github.com>
2025-08-02 16:18:14 +0530

    Problem 18. 4Sum solution

---
+++ src/main/java/Four_Sum_18.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        int n = nums.length;
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < n - 3; i++) {
+            // Skip duplicates for i
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            for (int j = i + 1; j < n - 2; j++) {
+                // Skip duplicates for j
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+
+                int p = j + 1;
+                int q = n - 1;
+
+                while (p < q) {
+                    long sum = (long) nums[i] + nums[j] + nums[p] + nums[q];
+
+                    if (sum < target) {
+                        p++;
+                    } else if (sum > target) {
+                        q--;
+                    } else {
+                        ans.add(Arrays.asList(nums[i], nums[j], nums[p], nums[q]));
+
+                        // Skip duplicates for p and q
+                        while (p < q && nums[p] == nums[p + 1]) p++;
+                        while (p < q && nums[q] == nums[q - 1]) q--;
+
+                        p++;
+                        q--;
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+}
