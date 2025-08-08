f37fb1af3e41941a07aac3a1c96c77de4fdbda13
Deepanshu Khandelwal <122585913+dkc2006@users.noreply.github.com>
2025-08-08 22:28:48 +0530

    Problem 102. Binary Tree Level Order Traversal solution

---
+++ src/main/java/BinaryTreeLevelOrderTraversal_102.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        helper(root, 0, ans);
+        return ans;
+    }
+
+    void helper(TreeNode root, int level, List<List<Integer>> ans){
+        if (root == null) return;
+        //add new array if not present
+        if (level == ans.size()){
+            ans.add(new ArrayList<>());
+        }
+        //add curr val
+        ans.get(level).add(root.val);
+
+        helper(root.left, level + 1 , ans);
+        helper(root.right, level+1 , ans);
+    }
+}
