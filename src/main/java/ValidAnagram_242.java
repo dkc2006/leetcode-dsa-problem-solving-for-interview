d1d9b8c60eae7ed15019c07f15a9458d021129fd
Deepanshu Khandelwal <122585913+dkc2006@users.noreply.github.com>
2025-09-05 14:09:18 +0530

    Problem 242. Valid Anagram solution

---
+++ src/main/java/ValidAnagram_242.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] charArray = s.toCharArray();
+        char[] charArray2 = t.toCharArray();
+
+        Arrays.sort(charArray);
+        Arrays.sort(charArray2);
+
+        return Arrays.equals(charArray, charArray2);
+    }
+}
