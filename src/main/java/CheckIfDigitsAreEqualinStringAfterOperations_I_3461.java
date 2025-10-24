71fb3ba44ce0a36ed0327d98e163f6b1bfc16136
Deepanshu Khandelwal <122585913+dkc2006@users.noreply.github.com>
2025-10-24 21:16:19 +0530

    Problem 3461. Check If Digits Are Equal in String After Operations I solution

---
+++ src/main/java/CheckIfDigitsAreEqualinStringAfterOperations_I_3461.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean hasSameDigits(String s) {
+        while (s.length() != 2){
+            StringBuilder temp = new StringBuilder();
+            for (int i = 1; i < s.length(); i++){
+                int sum = ((s.charAt(i - 1) - '0') + (s.charAt(i) - '0')) % 10;
+                temp.append(sum);
+            }
+            s = temp.toString();
+        }
+        return s.charAt(0) == s.charAt(1);
+    }
+}
